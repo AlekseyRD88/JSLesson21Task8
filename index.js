96c576765f54a9534dd1dc5df6918ae7a47a608a
= <neymegen50@gmail.com>
2022-02-13 11:34:44 +0200

    corrections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export function createButton(buttonText) {
   const elemButton = document.createElement('button');
-  elemButton.textContent = `<button>${buttonText}</button>`;
+  elemButton.textContent = buttonText;
   const elemBody = document.querySelector('body');
   elemBody.append(elemButton);
 }
